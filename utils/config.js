24825b6791ff0f677d7a84a9fb8054a9df19b2d7
DevStar7 <jacob.gavrilov716@gmail.com>
2022-02-15 13:34:45 +0800

    'update'

---
+++ utils/config.js
@@ -0,0 +1,19 @@
+{
+  "price": 0,
+  "number": 1000,
+  "gatekeeper": null,
+  "solTreasuryAccount": "CLYJM9h9yCx8NmbKpYBAaEocrryFPdjd6RkHVvjuViCK",
+  "splTokenAccount": null,
+  "splToken": null,
+  "goLiveDate": "15 Feb 2022 00:00:00 GMT",
+  "endSettings": null,
+  "whitelistMintSettings": null,
+  "hiddenSettings": null,
+  "storage": "arweave-sol",
+  "nftStorageKey": null,
+  "ipfsInfuraProjectId": null,
+  "ipfsInfuraSecret": null,
+  "awsS3Bucket": null,
+  "noRetainAuthority": false,
+  "noMutable": false
+}
